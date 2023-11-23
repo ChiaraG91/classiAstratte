9b3e961c425755c755c9426eabb7a3fc2dbc0964
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2023-11-23 10:17:14 +0100

    caricamento esercizio

---
+++ src/Forma.java
@@ -0,0 +1,23 @@
+public abstract class Forma {
+
+    public String nomeForma;
+
+
+    public Forma(String nomeForma){
+        this.nomeForma = nomeForma;
+    }
+
+    public String getNomeForma() {
+        return nomeForma;
+    }
+
+    public void setNomeForma(String nomeForma) {
+        this.nomeForma = nomeForma;
+    }
+
+    public void calcolaArea(){
+        System.out.println();
+    }
+}
+
+
